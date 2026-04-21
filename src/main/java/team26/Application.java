7a9fcd857c93a3a7d9893e88f186e7b1addc46c9
Alexander Skotnikov <92207202+merlich21@users.noreply.github.com>
2026-04-21 21:01:35 +0300

    fixed conflicts in pom.xml

--- src/main/java/team26/Application.java
+++ src/main/java/team26/Application.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Application {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
