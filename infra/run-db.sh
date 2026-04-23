a6882d3a4d5663178246186d4b12104df007a869
Egor <seniorpetrenko@gmail.com>
2026-04-23 18:23:49 +0300

    Исправление в Dockerfile для БД и в скриптах запуска БД в контейнере

--- infra/run-db.sh
+++ infra/run-db.sh
@@ -12,7 +12,7 @@ if [ $(ls -la | grep run-db.sh | wc -l) -gt 0 ]; then
     docker pull postgres:18.3-alpine
     docker build -t hackathon_postgres -f db-dockerfile .
     echo -e "\nThe 'hackathon_postgres' should be built\n"
-    docker run --name hackathon_postgres -p 5432:5432 -e POSTGRES_PASSWORD=$password -v postgres_data:/var/lib/postgresql/18/docker -v postgres_main:/var/lib/postgresql -d hackathon_postgres:latest
+    docker run --name hackathon_postgres -p 7432:5432 -e POSTGRES_PASSWORD=!password! -e POSTGRES_DB=testDB -v postgres_data:/var/lib/postgresql/18/docker -v postgres_main:/var/lib/postgresql -d hackathon_postgres:latest
     echo -e "\n_____INFORMATION:_____"
     echo -e "\nThe 'hackathon_postgres' container should be running. Use 127.0.0.1:5432 to connect to PostgreSQL. Login: postgres, DB: postgres"
     echo "Use 'docker start hackathon_postgres' command to start container, if it stops"
